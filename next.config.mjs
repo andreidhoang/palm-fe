b07ff16b3559f7aa3dd07f5b28b36e2f363c43ab
Dang Huy Hoang <dang_huy.hoang@student.unsw.edu.au>
2025-09-30 10:07:31 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imgs.search.brave.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
